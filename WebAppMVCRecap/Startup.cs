7c19bb41326a96e42232f87d4bf5b29339848395
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-04-04 10:14:50 +0200

    small fix

--- WebAppMVCRecap/Startup.cs
+++ WebAppMVCRecap/Startup.cs
@@ -25,8 +25,8 @@ namespace WebAppMVCRecap
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("BasicContext")));
+            //services.AddDbContext<DbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("BasicContext")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
