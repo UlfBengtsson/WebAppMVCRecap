4e0e34072637342c9f33cdd198f9bf014762400b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-04-11 16:26:27 +0200

    End of workshop

--- WebAppMVCRecap/Controllers/AccountController.cs
+++ WebAppMVCRecap/Controllers/AccountController.cs
@@ -12,12 +12,15 @@ namespace WebAppMVCRecap.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AccountController(UserManager<IdentityUser> userManager, 
+        public AccountController(RoleManager<IdentityRole> roleManager,
+                                 UserManager<IdentityUser> userManager,
                                  SignInManager<IdentityUser> signInManager)// Constructor Injection´s
         {
+            _roleManager = roleManager;
             _userManager = userManager;
             _signInManager = signInManager;
         }
@@ -55,7 +58,7 @@ namespace WebAppMVCRecap.Controllers
                 }
             }
 
-            
+
 
             return View(login);
 
@@ -84,7 +87,7 @@ namespace WebAppMVCRecap.Controllers
             }
             **/
         }
-    
+
 
         public async Task<IActionResult> Logout()
         {
@@ -93,27 +96,79 @@ namespace WebAppMVCRecap.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        [AllowAnonymous]//temp
+
         [HttpGet]
         public IActionResult CreateUser()
         {
             return View();
         }
-        [AllowAnonymous]//temp
+
         [HttpPost]
         public async Task<IActionResult> CreateUser(CreateUserVM createUser)
         {
             if (ModelState.IsValid)
             {
                 IdentityUser user = new IdentityUser() { UserName = createUser.UserName, Email = createUser.Email };
-                await _userManager.CreateAsync(user, createUser.Password);
+                var result = await _userManager.CreateAsync(user, createUser.Password);
+
+                if (result.Succeeded)
+                {
+                    ViewBag.msg = "User was created.";
+                    return RedirectToAction("CreateUser");
+                }
+                else
+                {
+                    ViewBag.errorlist = result.Errors;
+                }
             }
 
             return View(createUser);
         }
 
+        public IActionResult RoleList()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
 
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRole(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return View();
+            }
 
+            var result = await _roleManager.CreateAsync(new IdentityRole(name));
 
+            if (result.Succeeded)
+            {
+                return RedirectToAction("RoleList");
+            }
+
+            return View(name);
+        }
+
+        [HttpGet]
+        public IActionResult AddUserToRole(string role)
+        {
+            ViewBag.Role = role;
+
+            return View(_userManager.Users.ToList());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddUserToRoleSave(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            var result = await _userManager.AddToRoleAsync(user, role);
+
+            return RedirectToAction(nameof(RoleList));
+        }
     }
 }
